7ec11a9f1a9137c2a0fa8ca841177d06b76f6875
NguyenDat99 <donguyen5548@gmail.com>
2020-11-18 15:04:52 +0700

    commit 1

---
+++ model/predict.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+model = tf.keras.models.load_model('model/DenseNet201_160x160.h5')
+def predict(data):
+    ###  IMAGE PREPROCESSING  ###
+
+    #  data-->preprocessed_data #
+    
+    ###          END          ###
+    prediction = model.predict(preprocessed_data)
+    
+    # You may want to further format the prediction to make it more
+    # human readable
+    return prediction
