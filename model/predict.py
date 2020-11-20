14dc9e6a680148f03b1327d3c6815a3518e2f143
NguyenDat99 <donguyen5548@gmail.com>
2020-11-20 14:50:08 +0700

    cm2

--- model/predict.py
+++ model/predict.py
@@ -1,13 +1,25 @@
+import base64
+import numpy as np
+import pandas as pd 
 import tensorflow as tf
+
+IMAGE_WIDTH = 160
+IMAGE_HEIGHT = 160
+IMAGE_SIZE=(IMAGE_WIDTH, IMAGE_HEIGHT)
 model = tf.keras.models.load_model('model/DenseNet201_160x160.h5')
+
 def predict(data):
     ###  IMAGE PREPROCESSING  ###
-
+    fh = open("predict.jpg", "wb")
+    fh.write(base64.b64decode(data))
+    fh.close()
     #  data-->preprocessed_data #
-    
+    image = tf.keras.preprocessing.image.load_img('predict.jpg',target_size=IMAGE_SIZE)
+    input_arr = tf.keras.preprocessing.image.img_to_array(image)
+    input_arr = np.array([input_arr])  # Convert single image to a batch.
+    input_arr = input_arr/255
     ###          END          ###
-    prediction = model.predict(preprocessed_data)
-    
+    prediction = model.predict(input_arr)
     # You may want to further format the prediction to make it more
     # human readable
     return prediction
