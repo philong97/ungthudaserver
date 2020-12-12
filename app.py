5cd5d0a0a65318ffc2453dfd291622bd7ed4252e
NguyenDat99 <donguyen5548@gmail.com>
2020-12-12 21:40:30 +0700

    1

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
-from flask import Flask,request
-from model import predict
-# create the flask object
+import os
+from tensorflow import keras
+from flask import Flask, jsonify
+from gevent.pywsgi import WSGIServer
+import urllib.request
+
 app = Flask(__name__)
-@app.route('/')
-def index():
-    return "Index Page"
-@app.route('/predict',methods=['GET','POST'])
-def predict():
-    data = request.form.get('data')
-    if data == None:
-        return 'Got None'
-    else:
-        # model.predict.predict returns a dictionary
-        prediction = model.predict.predict(data) 
-    return json.dumps(str(prediction))
-if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True)
+
+@app.route("/")
+def hello1():
+	return jsonify({"Label":"Hello1"})
+
+@app.route("/<urlImg>")
+def hello(urlImg):
+	# keras.backend.clear_session()
+	model = keras.models.load_model('models/DenseNet201_160x160.h5')
+	return jsonify({"Label":1})
+
+if __name__ == '__main__':
+	#app.run(debug=True)
+    # Serve the app with gevent
+    http_server = WSGIServer(('0.0.0.0', 5000), app)
+    http_server.serve_forever()
